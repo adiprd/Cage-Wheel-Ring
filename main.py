c30dce21aff1bbc3ab7026e3c8cce1d1e2a37219
adip <146051948+adiprd@users.noreply.github.com>
2025-11-20 10:26:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 import io
-from PIL import Image
+import requests
 import supervision as sv
+from PIL import Image
 from rfdetr import RFDETRSegPreview
+from rfdetr.util.coco_classes import COCO_CLASSES
 
-MODEL_PATH = "model/weights.pt"
-IMAGE_PATH = "D:\Adip\Object Counting\Cropped\cropps\IMG-20251010-WA0031_cage_wheel_ring_1.jpg"
-CLASS_NAME = "cage_wheel_ring"
-THRESHOLD_CONF = 0.60
-THRESHOLD_NMS = 0.4
-MIN_MASK_AREA = 1000  
-
-model = RFDETRSegPreview(pretrain_weights=MODEL_PATH)
+# Load model
+model = RFDETRSegPreview(pretrain_weights='model/weights.pt')
 model.optimize_for_inference()
 
-image = Image.open(IMAGE_PATH)
+# Load image
+url = "D:\Adip\Object Counting\Cropped\crops\cage_wheel_ring_0.jpg"
+image = Image.open(url)
 
-detections = model.predict(image, threshold=THRESHOLD_CONF)
+# Predict dengan threshold
+detections = model.predict(image, threshold=0.4)  
 
-detections = detections.with_nms(threshold=THRESHOLD_NMS, class_agnostic=True)
-
-if len(detections) > 0 and hasattr(detections, "mask"):
-    mask_areas = [mask.sum() for mask in detections.mask]
-    keep_indices = [i for i, area in enumerate(mask_areas) if area > MIN_MASK_AREA]
-    detections = detections[keep_indices]
-
-detections.class_id = [0] * len(detections)
-CUSTOM_CLASSES = [CLASS_NAME]
-
+# Labels
 labels = [
-    f"{CUSTOM_CLASSES[class_id]} {confidence:.2f}"
-    for class_id, confidence in zip(detections.class_id, detections.confidence)
+    f"{COCO_CLASSES[class_id]} {confidence:.2f}"
+    for class_id, confidence
+    in zip(detections.class_id, detections.confidence)
 ]
 
+# Annotate
 annotated_image = image.copy()
 annotated_image = sv.MaskAnnotator().annotate(annotated_image, detections)
 annotated_image = sv.LabelAnnotator().annotate(annotated_image, detections, labels)
 
 sv.plot_image(annotated_image)
-
-object_count = len(detections)
-total_mask_area = sum(mask.sum() for mask in detections.mask) if len(detections) > 0 else 0
-
-print(f"Detected objects: {object_count}")
-print(f"Total segmented mask area: {total_mask_area}")
-print(f"Average mask area per object: {total_mask_area / object_count if object_count > 0 else 0:.2f}")
